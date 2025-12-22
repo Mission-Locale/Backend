1a733049ad7fade905c7e9dc31936fed999688f4
Syl2010 <sylvain.meunier13@gmail.com>
2025-12-22 18:36:57 +0100

    moving microsoft fetch options

--- src/repositories/MicrosoftService.js
+++ src/repositories/MicrosoftService.js
@@ -3,71 +3,73 @@ import { ClientSecretCredential } from "@azure/identity";
 import { Client } from "@microsoft/microsoft-graph-client";
 import { TokenCredentialAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials/index.js';
 
-
 class MicrosoftService {
-    client;
-    microsoftAccount = process.env.MICROSOFT_ACCOUNT;
-    clientId = process.env.MICROSOFT_APPLICATION_ID;
-    tenantId = process.env.MICROSOFT_TENANT_ID;
-    clientSecret = process.env.MICROSOFT_SECRET;
-    callendarId;
+  client;
+  microsoftAccount = process.env.MICROSOFT_ACCOUNT;
+  clientId = process.env.MICROSOFT_APPLICATION_ID;
+  tenantId = process.env.MICROSOFT_TENANT_ID;
+  clientSecret = process.env.MICROSOFT_SECRET;
+  callendarId;
 
-    constructor() {
-        const clientSecret = new ClientSecretCredential(
-            this.tenantId,
-            this.clientId,
-            this.clientSecret,
-        )
-        const authProvider = new TokenCredentialAuthenticationProvider(
-            clientSecret, {
-            scopes: ['https://graph.microsoft.com/.default'],
+  constructor() {
+    const clientSecret = new ClientSecretCredential(
+      this.tenantId,
+      this.clientId,
+      this.clientSecret
+    );
+    const authProvider = new TokenCredentialAuthenticationProvider(
+      clientSecret,
+      {
+        scopes: ["https://graph.microsoft.com/.default"],
+      }
+    );
+    const fetchOptions = {
+      headers: { Prefer: 'outlook.timezone="Europe/Paris"' },
+    };
+    this.client = Client.initWithMiddleware({ authProvider, fetchOptions });
+  }
+
+  async getCallendars() {
+    return await this.client
+      .api(`/users/${this.microsoftAccount}/calendars`)
+      .header("Prefer", 'outlook.timezone="Europe/Paris"')
+      .top(10)
+      .get();
+  }
+
+  async getCallendarId() {
+    if (!this.callendarId) {
+      this.callendarId = (await this.getCallendars()).value[0].id;
+    }
+    return this.callendarId;
+  }
+
+  async getEvents() {
+    const id = await this.getCallendarId();
+    return await this.client
+      .api(`/users/${this.microsoftAccount}/calendars/${id}/events`)
+      .header("Prefer", 'outlook.timezone="Europe/Paris"')
+      .top(10)
+      .get();
+  }
+
+  async getSchedule(start, end, duration = 60) {
+    const id = await this.getCallendarId();
+    return await this.client
+      .api(`/users/${this.microsoftAccount}/calendars/${id}/getSchedule`)
+      .post({
+        schedules: [this.microsoftAccount],
+        startTime: {
+          dateTime: start.toISOString(),
+          timeZone: "Europe/Paris",
         },
-        );
-
-        this.client = Client.initWithMiddleware({ authProvider })
-    }
-
-    async getCallendars() {
-        return await this.client
-            .api(`/users/${this.microsoftAccount}/calendars`)
-            .header("Prefer", 'outlook.timezone="Europe/Paris"')
-            .top(10)
-            .get()
-    }
-
-    async getCallendarId() {
-        if (!this.callendarId) {
-            this.callendarId = (await this.getCallendars()).value[0].id;
-        }
-        return this.callendarId;
-    }
-
-    async getEvents() {
-        const id = await this.getCallendarId();
-        return await this.client
-            .api(`/users/${this.microsoftAccount}/calendars/${id}/events`)
-            .header("Prefer", 'outlook.timezone="Europe/Paris"')
-            .top(10)
-            .get();
-    }
-
-    async getSchedule(start, end, duration = 60) {
-        const id = await this.getCallendarId();
-        return await this.client
-            .api(`/users/${this.microsoftAccount}/calendars/${id}/getSchedule`)
-            .post({
-                schedules: [this.microsoftAccount],
-                startTime: {
-                    dateTime: start.toISOString(),
-                    timeZone: 'Europe/Paris',
-                },
-                endTime: {
-                    dateTime: end.toISOString(),
-                    timeZone: 'Europe/Paris',
-                },
-                availabilityViewInterval: duration,
-            });
-    }
+        endTime: {
+          dateTime: end.toISOString(),
+          timeZone: "Europe/Paris",
+        },
+        availabilityViewInterval: duration,
+      });
+  }
 }
 
 export default new MicrosoftService;
